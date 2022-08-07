649b82b3fa8e23c64d4f473c0fa26fa5b75a9121
Balai Mikheeva <balaishka@yandex.ru>
2022-08-07 22:33:49 +0300

    feat: users

--- app.js
+++ app.js
@@ -26,7 +26,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const CastError = require('../errors/cast-err');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
 const userSchema = new mongoose.Schema({
